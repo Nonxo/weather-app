fb404b56a8e6f5007a9e817dbffecfd3ffd76760
Nonso Nwabuokei <nnwabuokei@gmail.com>
2021-08-07 19:40:56 +0100

    Make weather cards responsive

--- src/redux/service.js
+++ src/redux/service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getWeatherData = (unit) => {
   return axios
     .get(
-      `http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&units=${unit}&APPID=32946e5f4d8174ff9b9dccb19bd0fefe&cnt=40`
+      `https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&units=${unit}&APPID=32946e5f4d8174ff9b9dccb19bd0fefe&cnt=40`
     )
     .then((response) => response);
 };
